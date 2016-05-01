3dcb29798bb75b80fe690bbf690a1b8561c54b13
François Lecomte <francois.lecomte@gmail.com>
2016-05-01 21:24:48 +0200

    spring initializr

---
+++ src/main/java/com/github/lothar/hsystem/HSystemApplication.java
@@ -0,0 +1,12 @@
+package com.github.lothar.hsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/lothar/hsystem/HSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.lothar.hsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HSystemApplication.class)
+public class HSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
